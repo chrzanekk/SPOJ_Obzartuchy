6e944fe5dfea7aa7c5fff2412a54d77d31cb909f
Konrad <chrzanekk@gmail.com>
2018-10-29 21:26:55 +0100

    Update SPOJ_obzartuchy.py
    Trying to resolve this task. Almost done but some results are differents. Last part of code to check and fix.

--- SPOJ_obzartuchy.py
+++ SPOJ_obzartuchy.py
@@ -1,12 +1,11 @@
 import math
-testy = int(input()) #test count
+testy = int(input()) 
 for i in range(testy):
-    n = int(input()) #ilosc obzartuchow
-    m = int(input()) #ilosc ciastek
-    summary = 0
-    for j in range(n):
-        time = int(input()) #czas zjadania ciastek w sekundach
-        how_much = 86400/time #ile ciastek zjedza w ciagu 24h
-        summary += how_much
-    boxes = round(summary/m,2)
-    print(math.ceil(boxes))
+    N = int(input()) 
+    M = int(input()) 
+    ile = 0
+    for j in range(N):
+        t = int(input()) 
+        ile += (86400/t)
+    wynik = ile/M
+    print(math.ceil(wynik))
